cd2f58893587fd581f4a2a4375b5ccc74abc4e2b
Vins <shadowss.yt@gmail.com>
2020-07-07 15:05:42 +0200

    test

--- config/connect.php
+++ config/connect.php
@@ -1,10 +1,10 @@
 <?php
 $tokenpath = array(
-    'host' => '%dev',
-    'dbname' => 'blogtest',
+    'host' => 'localhost',
+    'dbname' => 'blogTest',
     'charset' => 'utf8',
-    'name' => 'dev',
-    'mdp' => 'dev7dev'    
+    'name' => 'Snows',
+    'mdp' => 'MoRello2598@#-=+'    
 );
 $bdd = new PDO('mysql:host='.$tokenpath['host'].';dbname='.$tokenpath['dbname'].';charset='.$tokenpath['charset'], $tokenpath['name'], $tokenpath['mdp']);
 $bdd->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
