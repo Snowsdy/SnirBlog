4946f978b3a846db28411f9a9c17f8416156d0fc
Vins <shadowss.yt@gmail.com>
2020-07-07 09:14:03 +0200

    Style pourri de l'article

--- README.md
+++ README.md
@@ -8,3 +8,7 @@ echo 'To learn Php';
 Enjoy :)
 ### What to do ?
 *Note: Page d'accueil terminée, reste page Admin, connection modal js, register, ...*
+#### Calendar :
+- 07/07/2020 : Writting register.php, perform article.php
+
+*__Issue__: Créer un 'header.php' comportant le menu ainsi que son style propre distinct des autres styles.*
--- article.php
+++ article.php
@@ -34,30 +34,39 @@ if (!isset($_GET['id']) or !is_numeric($_GET['id'])) {
     }
 
     $article = getArticle($id);
-    print_r($article, true);
     //$comments = getComments($id);
 }
 ?>
 
 <html lang="fr">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title><?= $article->title?></title>
     <link rel="stylesheet" href="css/article.css">
-    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css" integrity="sha384-9aIt2nRpC12Uk9gS9baDl411NQApFmC26EwAOH8WgZl5MYYxFfc+NcPb1dKGj7Sk" crossorigin="anonymous">
-    <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png">
-    <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png">
-    <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png">
-    <link rel="manifest" href="/site.webmanifest">
+    <link rel="apple-touch-icon" sizes="180x180" href="favicon_io/apple-touch-icon.png">
+    <link rel="icon" type="image/png" sizes="32x32" href="favicon_io/favicon-32x32.png">
+    <link rel="icon" type="image/png" sizes="16x16" href="favicon_io/favicon-16x16.png">
+    <link rel="manifest" href="favicon_io/site.webmanifest">
 </head>
+
 <body>
-<div class="container-fluid">
+    <div class="article">
         <a href="index.php" class="btn btn-submit" id="admin">Accueil</a>
-        <h1><?=$article->title?></h1>
-        <h2>By <?= $article->author ?></h2>
-        <time>Le <?= $article->publication_time ?></time>
-        <p><?=$article->content?></p>
+
+        <div class="pre-info">
+            <h1><?=$article->title?></h1>
+            <h2>By <?= $article->author ?></h2>
+            <time>Le <?= $article->publication_time ?></time>
+        </div>
+
+        <center><img src="<?= $article->path_img ?>"></center>
+
+        <div class="content">
+            <p><?=$article->content?></p>
+        </div>
+
         <hr />
 
         <?php 
@@ -65,25 +74,28 @@ if (!isset($_GET['id']) or !is_numeric($_GET['id'])) {
             echo $succes;
         }
         if(!empty($errors)):?>
-            <?php foreach ($errors as $error):?>
-                <div class="row">
-                    <div class="col-md-6">
-                        <div class="alert alert-danger"><?= $error ?></div>
-                    </div>
-                </div>
-            <?php endforeach;?>
+        <?php foreach ($errors as $error):?>
+        <div class="row">
+            <div class="col-md-6">
+                <div class="alert alert-danger"><?= $error ?></div>
+            </div>
+        </div>
+        <?php endforeach;?>
         <?php endif;?>
 
-        <!-- Espace Commentaires 
-        
-        <h2>Commentaires :</h2>
+        <div class="comments">
+            <h2>Commentaires :</h2>
 
-        <?php //foreach($comments as $com):?>
-            <h3><?php //$com->author ?></h3>
-            <time><?php //$com->date ?></time>
-            <p><?php //$com->comment ?></p>
-        <?php //endforeach;?> -->
+            <!-- Espace Commentaires 
+
+            <?php //foreach($comments as $com):?>
+                <h3><?php //$com->author ?></h3>
+                <time><?php //$com->date ?></time>
+                <p><?php //$com->comment ?></p>
+            <?php //endforeach;?> -->
+        </div>
 
     </div>
 </body>
+
 </html>
