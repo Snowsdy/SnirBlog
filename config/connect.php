217802a5d27928276f9bf04e94354ac0c44f64c6
dev <dev@mbp-de-dev.home>
2020-07-07 15:00:20 +0200

    test

--- config/connect.php
+++ config/connect.php
@@ -1,10 +1,10 @@
 <?php
 $tokenpath = array(
-    'host' => 'localhost',
-    'dbname' => 'blogTest',
+    'host' => '%dev',
+    'dbname' => 'blogtest',
     'charset' => 'utf8',
-    'name' => 'Snows',
-    'mdp' => 'MoRello2598@#-=+'    
+    'name' => 'dev',
+    'mdp' => 'dev7dev'    
 );
 $bdd = new PDO('mysql:host='.$tokenpath['host'].';dbname='.$tokenpath['dbname'].';charset='.$tokenpath['charset'], $tokenpath['name'], $tokenpath['mdp']);
 $bdd->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
