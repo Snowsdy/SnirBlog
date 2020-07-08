430d9e0d6df871b50b5c4b89931c0fc2726bca6d
dev <dev@mbp-de-dev.home>
2020-07-08 19:08:57 +0200

    header

--- index.php
+++ index.php
@@ -24,33 +24,9 @@ $articles = getArticles();
 </head>
 
 <body>
-    <div class="header">
-        <h2 class="logo"><span>Snir</span>Blog</h2>
-        <input type="checkbox" id="chk">
-        <label for="chk" class="show-menu-btn">
-            <i class="fas fa-ellipsis-h"></i>
-        </label>
-
-        <ul class="menu">
-            <?php if(isset($_SESSION['pseudo'])):?>
-            Bienvenue <b><?= strtoupper($_SESSION['pseudo']) ?></b>
-            <a href="login.php?logout">Se déconnecter</a>
-            <?php else:?>
-            <a id="login_btn">
-                <i class="fa fa-sign-in"></i> Se connecter
-            </a>
-            <a id="signUp_btn">Créer un compte</a>
-            <?php endif;?>
-            <?php if($_SESSION['admin']):?>
-            <a href="/admin/index.php">Tableau de bord</a>
-            <?php endif;?>
-            <a href="#">A propos</a>
-            <a href="#">Nous contacter</a>
-            <label for="chk" class="hide-menu-btn">
-                <i class="fas fa-times"></i>
-            </label>
-        </ul> 
-    </div>
+    <?php
+        include "php/header.php";
+    ?>
 
     <div class="content">
         <div class="post-articles">
