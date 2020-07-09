72c0d6d3a7648932daf75f5450e4b92ee3f53976
Vins <shadowss.yt@gmail.com>
2020-07-09 15:28:26 +0200

    e

--- article.php
+++ article.php
@@ -45,7 +45,7 @@ if (!isset($_GET['id']) or !is_numeric($_GET['id'])) {
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title><?= $article->title?></title>
-    <link rel="stylesheet" href="css/accueil.css">
+    <link rel="stylesheet" href="css/loginRegister.css">
     <link rel="stylesheet" href="admin/css/header.css">
     <link rel="stylesheet" href="css/article.css">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css" />
